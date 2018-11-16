b5d8bc1ee42cd39e316e5f9103d5397d6361220c
unknown <talvindersingh.ca@gmail.com>
2018-11-15 23:03:53 -0500

    Implementation for adding to Rinkeby Test network and metamask and testing the same

---
+++ deploy.js
@@ -0,0 +1,33 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+// Pass in the mnemonic and the tst newtork we want to connect to !! 
+const provider = new HDWalletProvider('master myself rail great tomato soft soda more present region clap crane',
+'https://rinkeby.infura.io/v3/49b0ba1b330d4f7ea92dd55fcb233a32'
+);
+
+// Enabled for rinkeby n/w.
+const web3 = new Web3(provider);
+
+// Need to make 2 async calls:
+// -- 1 to get the list of all accounts we have access to.
+// -- 2 to create and deploy the contract.
+// since there are 2, we can use a function to use async.
+const deploy = async() => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account:', accounts[0]);
+
+    // result : Holds the address where the contract got deployed.
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there']})
+    .send({gas: '1000000', from: accounts[0]});
+
+    console.log('Contract Deployed to :', result.options.address);
+    // Note: Contract Deployed to : 0xeA056b2948DC1C240Ce6432Aa0F4F4f688B598Ae
+    // this is needed to interface with any html/javascript application.
+    // Like loading this in same contract on remix.ethereum.org->Run->'At Address'
+    // and passing this address.
+};
+
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -9,7 +9,9 @@ const Web3 = require('web3'); // is a constructor fn or Class so is CAPITAL and
                               // used to create instances of web3 lib.
 // we can create multiple instances of Web3 to connect to different Ethereum networks.
 
-const web3 = new Web3(ganache.provider()); // connect to the local test n/w. on our machine.
+const provider = ganache.provider();// compatible with latest releases.
+const web3 = new Web3(provider); // connect to the local test n/w. on our machine.
+
 //To get access to the abi and bytecode fom contract 'Inbox'.
 const {interface, bytecode} = require('../compile'); 
 
@@ -27,7 +29,10 @@ beforeEach(()=>{
 });*/
 
 let accounts;
-let inbox;
+let inbox; // this object represents what is there on the blockchain.
+
+const INITIAL_MESSAGE = "Hi there";
+
 // Better way is to use Async await syntax for the above method.
 beforeEach(async ()=>{
     // get a list of all accounts.
@@ -36,20 +41,50 @@ beforeEach(async ()=>{
     accounts = await web3.eth.getAccounts();
     // use of these to deploy contract. And for that we need access to the 
     // bytecode.
-    inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
+    inbox = await new web3.eth.Contract(JSON.parse(interface)) // tells web3 about
+                                                  // what methods an Inbox contract has.
+                                                  // Contract is a contructor fn.
+    .deploy({ // Tells web3 that we want to deploy a new copy of this contract.
+              // with the given arguments.
         data: bytecode, // bytecode
-        arguments: ['Hi there'] // initial message
+        arguments: [INITIAL_MESSAGE] // initial message, its an array to accomodate more args
+                                // to the constructor fn of contract.
     })
-    .send({
+    .send({ // Instructs web3 to send out a transaction that creates this contract.
         from: accounts[0], // the first account from all.
         gas: '1000000' // gas .
     });
+
+    inbox.setProvider(provider);
+    /*
+    simple syntax:
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there']})
+    .send({from: accounts[0], gas: '1000000'});
+    */
 });
 
 describe('Inbox', () => {
-    it('deploys a contract', () =>{
-        console.log(inbox);
+    it('deploys a contract', () => {
+        //console.log(inbox);
+        // std node library.
+        assert.ok(inbox.options.address);
+    });
 
+    it('has a default message', async() => {
+        const message = await inbox.methods.message().call();// Call is read operations.
+        assert.equal(message, INITIAL_MESSAGE);
+    });
+
+    it('can change the message', async() => {
+        // transaction methods return a transation hash back.
+        await inbox.methods.setMessage('Updated').send( // send is a transaction operation.
+            // specify who will pay the gas for the modifier method.
+            {from: accounts[0]}
+            );
+        
+        // Get the updated message back.
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Updated');
     });
 });
